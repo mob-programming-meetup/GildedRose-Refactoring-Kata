62676b62ebac36514722896355617b126c2bb047
Mob-Programming Meetup Austria <69803388+Mob-Programming-Meetup-Austria@users.noreply.github.com>
2022-04-30 11:40:24 +0200

    merge ifs and introduce increment and decrement

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -4,7 +4,7 @@ package com.gildedrose;
  * - merge if conditions
  * - foreach
  * - use auto-refactoring tools
- * -
+ * - "String include in List"
  */
 
 class GildedRose {
@@ -19,46 +19,40 @@ class GildedRose {
             if (!item.name.equals("Aged Brie")
                 && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (item.quality > 0 && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    item.quality = item.quality - 1;
+                    item.quality--;
                 }
             } else {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+                    item.quality++;
 
                     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
+                        if (item.sellIn < 11 && item.quality < 50) {
+                            item.quality++;
                         }
 
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
+                        if (item.sellIn < 6 && item.quality < 50) {
+                            item.quality++;
                         }
                     }
                 }
             }
 
             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sellIn = item.sellIn - 1;
+                item.sellIn--;
             }
 
             if (item.sellIn < 0) {
                 if (!item.name.equals("Aged Brie")) {
                     if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                item.quality = item.quality - 1;
-                            }
+                        if (item.quality > 0 && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            item.quality--;
                         }
                     } else {
-                        item.quality = item.quality - item.quality;
+                        item.quality = 0;
                     }
                 } else {
                     if (item.quality < 50) {
-                        item.quality = item.quality + 1;
+                        item.quality++;
                     }
                 }
             }
